04da0fe8299135445b2210a4928227dc9e44e842
parkerray <parkerthompson@hey.com>
2023-07-07 10:08:12 -0500

    updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import type { Metadata } from 'next'
- import './globals.css'
- 
+import type { Metadata } from "next";
+import "./globals.css";
+
 export const metadata: Metadata = {
-  title: 'game',
-  description: '01101001 01110100 00100000 01110111 01101111 01101110 01110100 00100000 01100010 01100101 00100000 01110100 01101000 01100001 01110100 00100000 01100101 01100001 01110011 01111001',
-}
- 
+  title: "cab-eacf",
+  description:
+    "01101001 01110100 00100000 01110111 01101111 01101110 01110100 00100000 01100010 01100101 00100000 01110100 01101000 01100001 01110100 00100000 01100101 01100001 01110011 01111001",
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className='bg-black text-white font-mono'>
-        {children}
-      </body>
+    <html lang='en'>
+      <body className='bg-black text-white font-mono'>{children}</body>
     </html>
-  )
+  );
 }
