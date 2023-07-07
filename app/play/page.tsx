efb5b56dbeb9756d4c557adbfe03a117e2baae39
parkerray <parkerthompson@hey.com>
2023-07-07 10:16:47 -0500

    added favicon

--- app/play/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Play() {
-  return (
-    <div className='flex flex-col items-center justify-center min-h-screen min-w-full p-8'>
-      <p>this is where we play</p>
-    </div>
-  );
-}
